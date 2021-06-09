1363e9224d3cc0cae01befa1cd42be16998c6970
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-09 19:07:05 +0530

    updated nuxt.config.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,16 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Lora&family=Quicksand:wght@300;400&display=swap',
+      },
+    ],
   },
-  css: [],
-  plugins: [],
+  css: ['assets/global.scss'],
   components: true,
   buildModules: [
     // https://go.nuxtjs.dev/eslint
@@ -21,6 +27,4 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
-  axios: {},
-  build: {},
 }
