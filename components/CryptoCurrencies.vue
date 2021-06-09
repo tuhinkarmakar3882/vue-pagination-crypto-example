7e26aa0d91118ce9141204b0d3c2aa05d035d792
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-09 19:39:03 +0530

    Added header
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/CryptoCurrencies.vue
+++ components/CryptoCurrencies.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="crypto-currencies-component">
-    <section
-      v-for="cryptoCurrency in cryptoCurrencies"
-      :key="cryptoCurrency.id"
-      tabindex="0"
-    >
-      <img :alt="cryptoCurrency.name" :src="cryptoCurrency.iconUrl" />
-      <aside>
-        <p><strong>Name:</strong> {{ cryptoCurrency.name }}</p>
-        <p><strong>Price:</strong> ${{ cryptoCurrency.price }}</p>
-        <p><strong>Change:</strong> {{ cryptoCurrency.change }}%</p>
-      </aside>
-    </section>
+    <header>
+      <button>Previous</button>
+      <select v-model="itemsPerPage" name="items-per-page">
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
+      <button>Next</button>
+    </header>
+
+    <main>
+      <section
+        v-for="cryptoCurrency in cryptoCurrencies.slice(0, itemsPerPage)"
+        :key="cryptoCurrency.id"
+        tabindex="0"
+      >
+        <img :alt="cryptoCurrency.name" :src="cryptoCurrency.iconUrl" />
+        <aside>
+          <p><strong>Name:</strong> {{ cryptoCurrency.name }}</p>
+          <p><strong>Price:</strong> ${{ cryptoCurrency.price }}</p>
+          <p><strong>Change:</strong> {{ cryptoCurrency.change }}%</p>
+        </aside>
+      </section>
+    </main>
   </div>
 </template>
 
@@ -23,7 +35,13 @@ export default {
   data() {
     return {
       cryptoCurrencies: undefined,
+      itemsPerPage: 25,
     }
+  },
+  watch: {
+    itemsPerPage(newValue) {
+      // this.$store.dispatch('Currencies/')
+    },
   },
   mounted() {
     this.cryptoCurrencies = this.fetchCryptoData()
@@ -38,37 +56,45 @@ export default {
 
 <style lang="scss" scoped>
 .crypto-currencies-component {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-
-  section {
+  header {
     display: flex;
-    background: hsl(0, 0%, 90%);
-    gap: 16px;
-    flex: 1 1 0;
-    min-width: 250px;
-    padding: 1rem;
-    border-radius: 8px;
-    box-shadow: 0 0 4px hsla(0, 0%, 0%, 0.3);
-    transition: all 200ms ease-in-out;
-    cursor: pointer;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2rem;
+  }
 
-    &:hover,
-    &:focus {
-      transform: scale(1.05);
-      background: hsl(100, 40%, 90%);
-    }
+  main {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--standard);
 
-    &:active {
-      background: hsl(100, 40%, 90%);
-      transform: scale(0.9);
-    }
+    section {
+      display: flex;
+      background: var(--card-background);
+      gap: 16px;
+      flex: 1 1 0;
+      min-width: 250px;
+      padding: var(--standard);
+      border-radius: 8px;
+      box-shadow: 0 0 4px hsla(0, 0%, 0%, 0.3);
+      cursor: pointer;
 
-    img {
-      height: 64px;
-      width: 64px;
-      object-fit: cover;
+      &:hover,
+      &:focus {
+        transform: scale(1.05);
+        background: var(--primary-color);
+      }
+
+      &:active {
+        background: var(--primary-color);
+        transform: scale(0.95);
+      }
+
+      img {
+        height: 64px;
+        width: 64px;
+        object-fit: cover;
+      }
     }
   }
 }
